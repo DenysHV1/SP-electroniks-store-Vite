7b682319ebd1bfe719c9772239ea929905fb1445
DenVik <denyagarkusha2@gmail.com>
2024-08-02 15:15:54 +0200

    try fix problem with link to img

--- src/js/array-slider-pictures.js
+++ src/js/array-slider-pictures.js
@@ -1,32 +1,32 @@
-// export const itemsSlider = [
-// 	{
-// 		hash: 'slider1',
-// 		src1: '/img/slider-1-1x.jpg',
-// 		src2: '/img/slider-1-2x.jpg',
-// 		alt: 'slider1'
-// 	},
-// 	{
-// 		hash: 'slider2',
-// 		src1: '/img/slider-2-1x.jpg',
-// 		src2: '/img/slider-2-2x.jpg',
-// 		alt: 'slider2'
-// 	},
-// 	{
-// 		hash: 'slider3',
-// 		src1: '/img/slider-3-1x.jpg',
-// 		src2: '/img/slider-3-2x.jpg',
-// 		alt: 'slider3'
-// 	},
-// 	{
-// 		hash: 'slider4',
-// 		src1: '/img/slider-4-1x.jpg',
-// 		src2: '/img/slider-4-2x.jpg',
-// 		alt: 'slider4'
-// 	},
-// 	{
-// 		hash: 'slider5',
-// 		src1: '/img/slider-5-1x.jpg',
-// 		src2: '/img/slider-5-2x.jpg',
-// 		alt: 'slider4'
-// 	},
-// ]
+export const itemsSlider = [
+	{
+		hash: 'slider1',
+		src1: '../img/slider-1-1x.jpg',
+		src2: '../img/slider-1-2x.jpg',
+		alt: 'slider1'
+	},
+	{
+		hash: 'slider2',
+		src1: '../img/slider-2-1x.jpg',
+		src2: '../img/slider-2-2x.jpg',
+		alt: 'slider2'
+	},
+	{
+		hash: 'slider3',
+		src1: '../img/slider-3-1x.jpg',
+		src2: '../img/slider-3-2x.jpg',
+		alt: 'slider3'
+	},
+	{
+		hash: 'slider4',
+		src1: '../img/slider-4-1x.jpg',
+		src2: '../img/slider-4-2x.jpg',
+		alt: 'slider4'
+	},
+	{
+		hash: 'slider5',
+		src1: '../img/slider-5-1x.jpg',
+		src2: '../img/slider-5-2x.jpg',
+		alt: 'slider4'
+	},
+]
