432e0b005a47757b2c698d202c6fc3d731e8c9bf
DenVik <denyagarkusha2@gmail.com>
2024-08-02 16:07:19 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -1,38 +1,4 @@
-// import { itemsSlider } from './js/array-slider-pictures';
-const itemsSlider = [
-		{
-			hash: 'slider1',
-			src1: "./img/slider-1-1x.jpg",
-			src2: "./img/slider-1-2x.jpg",
-			alt: 'slider1'
-		},
-		{
-			hash: 'slider2',
-			src1: "./img/slider-2-1x.jpg",
-			src2: "./img/slider-2-2x.jpg",
-			alt: 'slider2'
-		},
-		{
-			hash: 'slider3',
-			src1: "./img/slider-3-1x.jpg",
-			src2: "./img/slider-3-2x.jpg",
-			alt: 'slider3'
-		},
-		{
-			hash: 'slider4',
-			src1: "./img/slider-4-1x.jpg",
-			src2: "./img/slider-4-2x.jpg",
-			alt: 'slider4'
-		},
-		{
-			hash: 'slider5',
-			src1: "./img/slider-5-1x.jpg",
-			src2: "./img/slider-5-2x.jpg",
-			alt: 'slider4'
-		},
-	]
-import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
+import { itemsSlider } from './js/array-slider-pictures';
 const swiperWrapper = document.querySelector('.swiper-wrapper');
 
 const sliderMarkup = itemsSlider.map(
@@ -54,7 +20,8 @@ class="swiper__img"
 </div>`
 );
 swiperWrapper.insertAdjacentHTML('afterbegin', sliderMarkup);
-
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 new Swiper('.swiper', {
   navigation: {
     nextEl: '.swiper-button-next',
